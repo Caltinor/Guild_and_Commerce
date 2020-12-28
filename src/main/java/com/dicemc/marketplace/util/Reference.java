af14e8de14461efbb9a4fce6d338352ed4f58423
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-12-28 16:04:19 -0500

    version update for release

--- src/main/java/com/dicemc/marketplace/util/Reference.java
+++ src/main/java/com/dicemc/marketplace/util/Reference.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 public class Reference {
 	public static final String MOD_ID = "gnc";
 	public static final String NAME = "Guild & Commerce";
-	public static final String VERSION = "1.2.8";
+	public static final String VERSION = "1.2.8c";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.dicemc.marketplace.util.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.dicemc.marketplace.util.proxy.ServerProxy";
